6e630f46410c6bf98234b6e045be5c2a867a0283
Erik Hoffman <erik.johansson87@gmail.com>
2020-02-13 10:21:32 +0100

    changed the key generation

--- src/ratingsRepository.js
+++ src/ratingsRepository.js
@@ -1,15 +1,16 @@
 const redisClient = require("./helpers/redisClient");
 
-const KEY_ASSET = assetId => `ratings:${assetId}`;
+const KEY_PREFIX = "ratings";
+const generateKey = (...args) => args.join(":");
 
 const add = async (assetId, rating) => {
-  const key = KEY_ASSET(assetId);
+  const key = generateKey(KEY_PREFIX, assetId);
   await redisClient.lpush(key, rating);
   return true;
 };
 
 const get = async assetId => {
-  const key = KEY_ASSET(assetId);
+  const key = generateKey(KEY_PREFIX, assetId);
   const ratings = await redisClient.lrange(key, 0, -1);
   const sum = ratings.reduce((a, b) => Number(a) + Number(b), 0);
   const average = sum / ratings.length;
@@ -17,7 +18,7 @@ const get = async assetId => {
 };
 
 const amount = async assetId => {
-  const key = KEY_ASSET(assetId);
+  const key = generateKey(KEY_PREFIX, assetId);
   const ratingsList = await redisClient.lrange(key, 0, -1);
   return ratingsList.length;
 };
